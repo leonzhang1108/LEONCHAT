56f34e69a1cebdb79e5c4668844030a258fc328a
leon <jianliangzhang@ctrip.com>
2017-12-26 18:02:29 +0800

    样式修改

--- src/js/pages/Index.js
+++ src/js/pages/Index.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Layout } from 'antd'
 import 'style/pages/index'
+
+const { Header, Footer, Content } = Layout
+
 const Index = props => (
-  <div>index</div>
+  <Layout>
+    <Header>index header</Header>
+    <Content>index content</Content>
+    <Footer>index footer</Footer>
+  </Layout>
 )
 
 export default Index
