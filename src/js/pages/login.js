4bdd3bdc1f0019c4dc130d949c708e747f9ce547
leonzhang <human1108@163.com>
2017-12-28 02:19:54 +0800

    format

--- src/js/pages/login.js
+++ src/js/pages/login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'style/pages/login'
-import { Input } from 'antd'
+import { Input, Button } from 'antd'
 import { createSocket } from 'socket'
 import { observer, inject } from 'mobx-react'
 
@@ -12,16 +12,18 @@ class Login extends React.Component{
    constructor(props) {
      super(props)
    }
-   doLogin = name => {
 
+   doLogin = name => {
     const { store, history } = this.props
     store.changeNickname(name)
     store.addSocket(createSocket(name))
     history.push(`/chat`)
-
    }
+
    render() {
+    
     const { store } = this.props
+
     let renderDom = <Search placeholder="your nickname" onSearch={this.doLogin} enterButton="Login" />
     if(store.socket) renderDom = 'click to logout'
 
