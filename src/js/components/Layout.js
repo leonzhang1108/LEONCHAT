707a55f8175a7faefc92062a5311632b5ad5da59
leon <jianliangzhang@ctrip.com>
2017-12-29 13:35:57 +0800

    简写

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,15 +1,9 @@
 import { Flex } from 'antd'
 import React from 'react'
 import 'style/components/layout'
-const Layout = WrappedComponent => {
-  return class extends React.Component {
-    render() {
-      return (
-        <Flex justify="center" className="layout-flex">
-          <WrappedComponent {...this.props} />
-        </Flex>
-      )
-    }
-  }
-}
+const Layout = WrappedComponent => props => (
+  <Flex justify="center" className="layout-flex">
+    <WrappedComponent {...props} />
+  </Flex>
+)
 export default Layout
