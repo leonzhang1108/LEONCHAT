fd51b65d5180a0e3e37f2bf5e4df21b1f881043e
leonzhang <human1108@163.com>
2018-01-14 23:52:51 +0800

    404ICON 不让缩放

--- src/js/pages/NoMatch.js
+++ src/js/pages/NoMatch.js
@@ -8,7 +8,13 @@ import { observer, inject } from 'mobx-react'
 class NoMatch extends React.Component {
   render () {
     const { common } = this.props.store.locale
-    return <div>{common.noMatch}</div>
+    
+    return (
+      <div className='no-match-wraper'>
+        <i className='icon iconfont icon-404 font-404-icon'/> 
+        <div>{common.noMatch}</div>
+      </div>
+    )
   }
 }
 
